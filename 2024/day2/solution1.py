478ec3f8d873841331ff16523b5c2b64d3d811a9
Christopher Ward <cdurianward@gmail.com>
2024-12-13 11:18:46 -0800

    feat(day2): Solutions 1 and 2

---
+++ 2024/day2/solution1.py
@@ -0,0 +1,39 @@
+safe = 0
+
+def test_safety(report):
+    is_safe = True
+    prev = int(report[0])
+    is_increase = None
+
+    for i in range(1, len(report)):
+      curr = int(report[i])
+      if is_increase is None:
+        if curr > prev:
+          is_increase = True
+        elif curr < prev:
+          is_increase = False
+        else:
+          is_safe = False
+          break
+
+      if is_increase and curr < prev:
+        is_safe = False
+        break
+      if not is_increase and curr > prev:
+        is_safe = False
+        break
+      if abs(curr - prev) < 1 or abs(curr - prev) > 3:
+        is_safe = False
+        break
+      prev = curr
+
+    return is_safe
+
+
+with open("input.txt") as f:
+  for line in f:
+    report = line.rstrip().split(' ')
+    if test_safety(report):
+      safe += 1
+
+print(safe)
